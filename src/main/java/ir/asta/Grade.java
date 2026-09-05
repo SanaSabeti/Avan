9c269bf66f73e230c7ffd78b3cec4cdf0ff04cb3
SanaSabeti <sana.sabeti20@gmail.com>
2026-09-05 09:33:38 +0330

    Update Grade.java

--- src/main/java/ir/asta/Grade.java
+++ src/main/java/ir/asta/Grade.java
@@ -1,6 +1,11 @@
 package ir.asta;
 public enum Grade {
-    A(90), B(80), C(70), D(60), F(0);
+    A(90),
+    B(80),
+    C(70),
+    D(60),
+    F(0);
+
     private final int minScore;
 
     Grade(int i) {
