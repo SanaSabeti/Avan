5e9c71ba563f693abc7abbfde621070d64f51972
SanaSabeti <sana.sabeti20@gmail.com>
2026-09-06 10:55:37 +0330

    FSL-571: fix comment 1

--- src/main/java/ir/asta/PersonUtils.java
+++ src/main/java/ir/asta/PersonUtils.java
@@ -1,11 +1,12 @@
 package ir.asta;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PersonUtils {
     public static <T extends Person> T findByName(List<T> people, String name) {
         for (T person : people) {
-            if (person.getName().equals(name)) {
+            if (Objects.equals(person.getName(), name)) {
                 return person;
             }
         }
