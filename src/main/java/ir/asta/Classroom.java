af105b4e7495c49aa5ad96399bc93c6f8d335517
SanaSabeti <sana.sabeti20@gmail.com>
2026-09-08 09:42:06 +0330

    FSL-571: add javadoc

--- src/main/java/ir/asta/Classroom.java
+++ src/main/java/ir/asta/Classroom.java
@@ -8,6 +8,14 @@ public class Classroom implements Iterable<Student> {
     private Set<String> names = new HashSet<>();
     private HashMap<Integer, Student> studentsWithId = new HashMap<>();
 
+    /**
+     * Adds a student to the classroom
+     *
+     * @param student to add
+     * @return true if added
+     * @throws InvalidScoreException     if score is invalid
+     * @throws DuplicateStudentException if name is duplicated
+     */
     public boolean addStudent(Student student) throws InvalidScoreException {
         if (student.getScore() < 0 || student.getScore() > 100) {
             throw new InvalidScoreException("Invalid score!");
@@ -23,26 +31,48 @@ public class Classroom implements Iterable<Student> {
         return true;
     }
 
+    /**
+     * Adds a teacher.
+     *
+     * @param teacher to add
+     */
     public void addTeacher(Teacher teacher) {
         persons.add(teacher);
     }
 
+    /**
+     * @return average score
+     */
     public double average() {
         return students.stream().mapToInt(Student::getScore).average().orElse(0.0);
     }
 
+    /**
+     * @return highest score
+     */
     public int max() {
         return students.stream().mapToInt(Student::getScore).max().orElse(0);
     }
 
+    /**
+     * @return lowest score
+     */
     public int min() {
         return students.stream().mapToInt(Student::getScore).min().orElse(0);
     }
 
+    /**
+     * Adds points to all student scores.
+     *
+     * @param points to add
+     */
     public void curve(int points) {
         students.stream().forEach(student -> student.curveScore(points));
     }
 
+    /**
+     * Sorts students by score and prints their ranks.
+     */
     public void sortStudents() {
         students.sort((s1, s2) -> Integer.compare(s2.getScore(), s1.getScore()));
         List<Pair<Student, Integer>> rankedStudents = new ArrayList<>();
@@ -58,6 +88,11 @@ public class Classroom implements Iterable<Student> {
         }
     }
 
+    /**
+     * Prints a report of all classroom members (both students and teachers).
+     * For students, it also prints their grade and pass/fail status.
+     * At the end, it prints the average, maximum, and minimum scores.
+     */
     public void showReport() {
         for (Person person : persons) {
             System.out.println(person.describe());
@@ -74,14 +109,29 @@ public class Classroom implements Iterable<Student> {
         System.out.println("Minimum: " + min());
     }
 
+    /**
+     * Finds a student by ID using HashMap.
+     *
+     * @param id student ID
+     * @return student with the given ID, or null
+     */
     public Student findStudentWithIdMap(int id) {
         return studentsWithId.get(id);
     }
 
+    /**
+     * Finds a student by ID using linear search.
+     *
+     * @param id student ID
+     * @return student with the given ID, or null
+     */
     public Student findStudentWithIdLinear(int id) {
         return students.stream().filter(student -> student.getId() == id).findFirst().orElse(null);
     }
 
+    /**
+     * @return student iterator
+     */
     @Override
     public Iterator<Student> iterator() {
         return new Iterator<Student>() {
@@ -99,6 +149,12 @@ public class Classroom implements Iterable<Student> {
         };
     }
 
+    /**
+     * Adds multiple students.
+     *
+     * @param students to add
+     * @throws InvalidScoreException if a score is invalid
+     */
     public void addStudents(Student... students) throws InvalidScoreException {
         for (Student student : students) {
             addStudent(student);
--- src/main/java/ir/asta/Student.java
+++ src/main/java/ir/asta/Student.java
@@ -11,34 +11,58 @@ public class Student extends Person implements Gradable {
         System.out.println("Student class loaded");
     }
 
+    /**
+     * Creates a student with default name and score.
+     */
     public Student() {
         this("unknown", 0);
     }
 
+    /**
+     * Creates a student with a name and score.
+     *
+     * @param name  student name
+     * @param score student score
+     */
     public Student(String name, int score) {
         super(name);
         this.id = ++liveInstance;
         this.score = score;
     }
 
+    /**
+     * @return student score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * @return student ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "Student number" + id + ", name= " + getName() + ", score= " + score;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this) {
@@ -51,16 +75,30 @@ public class Student extends Person implements Gradable {
         return this.id == temp.id;
     }
 
+    /**
+     * @return student information
+     */
     @Override
     public String describe() {
         return super.showInfo() + ", Student ID: " + id + ", Score: " + score;
     }
 
+    /**
+     * Converts the student's score to a grade.
+     *
+     * @return grade based on the student's score
+     */
     @Override
     public Grade getGrade() {
         return Grade.fromScore(score);
     }
 
+    /**
+     * Adds points to the student's score.
+     * The score cannot be greater than 100.
+     *
+     * @param points points to add
+     */
     public void curveScore(int points) {
         score += points;
         if (score > 100) {
@@ -68,6 +106,11 @@ public class Student extends Person implements Gradable {
         }
     }
 
+    /**
+     * Checks if the student is passing.
+     *
+     * @return true if the student is passing
+     */
     @Override
     public boolean isPassing() {
         return getGrade() != Grade.F;
